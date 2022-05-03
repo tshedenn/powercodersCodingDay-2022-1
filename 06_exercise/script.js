a4991f33f08585077505c2716ca3e706d4af49bb
tshering <tshering.denkar@powercoders.org>
2022-05-03 11:36:55 +0200

    switch statments

--- 06_exercise/script.js
+++ 06_exercise/script.js
@@ -1,1 +1,35 @@
-let weekNumbersToWords 
+let weekNumbersToWords =()=>{
+    let weekNumbers=[1,2,3,4,5,6,7];
+    let day;
+    let weekDay=[];
+    switch(weekNumbers){
+      case 1:
+        day ='Monday';
+        weekDay.push(day);
+        break;
+      case 2:
+        day ='Tuesday';
+        weekDay.push(day);
+        break; 
+      case 3:
+        day='Wednesday';
+        weekDay.push(day);
+        break;
+      case 4:
+        day='Thursday';
+        weekDay.push(day);
+        break;  
+      case 5:
+        day ='Friday';
+        weekDay.push(day);
+        break;
+      case 6:
+        day = 'Saturday'
+        weekDay.push(day);  
+        break;
+      default:
+        day = 'invalid'; 
+    }
+
+}
+weekNumbersToWords();
