7baab63ebcf8d3eb98a554686b7303a77cca8abd
tshering <tshering.denkar@powercoders.org>
2022-05-03 20:35:21 +0200

    try different method

--- 07_exercise/script.js
+++ 07_exercise/script.js
@@ -5,3 +5,19 @@ let reverse = (number)=> {
 console.log(reverse(1234 ));
 console.log(reverse(10000));
 console.log(reverse(-321));
+
+// let reverseNum= (number) => {
+//   let stringconvert = number.toString();
+//   let splitNum = stringconvert.split('');
+//   let reveredNum = splitNum.reverse();
+//   let joinNum = reveredNum.join();
+//   console.log(stringconvert);
+//   console.log(splitNum);
+//   console.log(reveredNum);
+//   console.log(joinNum);
+//   return Math.sign(number) * parseInt(joinNum);
+
+  
+
+// }
+// reverseNum('100000');
